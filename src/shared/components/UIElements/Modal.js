18b518e4ec6ec7383ef5ec8ce45db0fa04be1098
mishkamag <m93.maglaperidze@gmail.com>
2023-03-11 23:44:37 +0400

    finished modal

--- src/shared/components/UIElements/Modal.js
+++ src/shared/components/UIElements/Modal.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
+import Backdrop from "./Backdrop";
 import "./Modal.css";
 
 const ModalOverlay = (props) => {
@@ -23,12 +25,24 @@ const ModalOverlay = (props) => {
       </form>
     </div>
   );
-
   return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
 };
 
 const Modal = (props) => {
-  return <div>Modal</div>;
+  return (
+    <React.Fragment>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </React.Fragment>
+  );
 };
 
 export default Modal;
